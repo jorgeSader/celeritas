a2eebfcd2de406d0a53ebed00e4c5cb72f548c11
JSader <jorge.sader@gmail.com>
2025-02-27 07:54:56 +0100

    feat(types): add initPaths struct for directory initialization

---
+++ types.go
@@ -0,0 +1,7 @@
+package celeritas
+
+// initPaths defines the root path and folder structure for initializing the application.
+type initPaths struct {
+	rootPath    string
+	folderNames []string
+}

ccb084a9236878abfd35fd739c75b3e5254dfa2d
JSader <jorge.sader@gmail.com>
2025-02-28 11:19:13 +0100

    test: write test for Render.Page function
     - add setup_test.go file
     - add mock go template page
     - add mock jet template page
     - write TestRender_page function to test page rendering functionality

---
+++ render/render_test.go
@@ -0,0 +1,32 @@
+package render
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRender_Page(t *testing.T) {
+	r, err := http.NewRequest("GET", "some-url", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	w := httptest.NewRecorder()
+
+	testRenderer.RootPath = "./test-data"
+	testRenderer.Renderer = "go"
+
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err != nil {
+		t.Error("Error rendering page", err)
+	}
+
+	testRenderer.Renderer = "jet"
+
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err != nil {
+		t.Error("Error rendering page", err)
+	}
+
+}

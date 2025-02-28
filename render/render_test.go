2a98c325bdf32174148432e36074391e123a6c24
JSader <jorge.sader@gmail.com>
2025-02-28 15:59:26 +0100

    test(render): Simplify Render.Page tests by using table tests.

--- render/render_test.go
+++ render/render_test.go
@@ -6,42 +6,43 @@ import (
 	"testing"
 )
 
+var pageData = []struct {
+	name          string
+	renderer      string
+	templateName  string
+	errorExpected bool
+	errorMessage  string
+}{
+	{"go_page", "go", "home", false, "error rendering go template"},
+	{"go_page", "go", "non-existing", true, "no error rendering non-existent go page when one is expected"},
+	{"jet_page", "jet", "home", false, "error rendering jet template"},
+	{"jet_page", "jet", "non-existing", true, "no error rendering non-existent jet page when one is expected"},
+	{"invalid_render_engine", "foo", "home", true, "No error rendering with invalid rendering engine"},
+}
+
 func TestRender_Page(t *testing.T) {
-	r, err := http.NewRequest("GET", "/some-url", nil)
-	if err != nil {
-		t.Error(err)
-	}
+	for _, e := range pageData {
+		r, err := http.NewRequest("GET", "/some-url", nil)
+		if err != nil {
+			t.Error(err)
+		}
 
-	w := httptest.NewRecorder()
+		w := httptest.NewRecorder()
 
-	testRenderer.RootPath = "./test-data"
+		testRenderer.RootPath = "./test-data"
+		testRenderer.Renderer = e.renderer
 
-	testRenderer.Renderer = "go"
-	err = testRenderer.Page(w, r, "home", nil, nil)
-	if err != nil {
-		t.Error("Error rendering go page", err)
+		err = testRenderer.Page(w, r, e.templateName, nil, nil)
+		if e.errorExpected {
+			if err == nil {
+				t.Errorf("%s: %s", e.name, e.errorMessage)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("%s: %s: %s", e.name, e.errorMessage, err.Error())
+			}
+		}
 	}
-	err = testRenderer.Page(w, r, "non-existent", nil, nil)
-	if err == nil {
-		t.Error("Error rendering non-existent go template", err)
-	}
-
-	testRenderer.Renderer = "jet"
-	err = testRenderer.Page(w, r, "home", nil, nil)
-	if err != nil {
-		t.Error("Error rendering jet page", err)
-	}
-	err = testRenderer.Page(w, r, "non-existent", nil, nil)
-	if err == nil {
-		t.Error("Error rendering non-existent jet template", err)
-	}
-
-	testRenderer.Renderer = ""
-	err = testRenderer.Page(w, r, "home", nil, nil)
-	if err == nil {
-		t.Error("No error returned while rendering with invalid renderer specified", err)
-	}
-
 }
 
 func TestRender_GoPage(t *testing.T) {

098bcb8b41e2a075dc8a1792c3c61cdd7ab817a7
JSader <jorge.sader@gmail.com>
2025-03-09 08:40:23 +0100

    feat(driver): upgrade to pgx/v5 database driver
    - Updated driver imports to use pgx/v5
    - Simplified import statements by removing unused v4 packages

--- driver.go
+++ driver.go
@@ -3,9 +3,7 @@ package celeritas
 import (
 	"database/sql"
 
-	_ "github.com/jackc/pgconn"
-	_ "github.com/jackc/pgx/v4"
-	_ "github.com/jackc/pgx/v4/stdlib"
+	_ "github.com/jackc/pgx/v5"
 )
 
 func (c *Celeritas) OpenDB(dbType, dsn string) (*sql.DB, error) {

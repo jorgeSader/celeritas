0ac9be7248a183a7d5dfaee6284d38bd93d45241
JSader <jorge.sader@gmail.com>
2025-03-12 19:41:42 +0100

    feat(core): add migration methods to Celeritas
    - Added migrations.go with MigrateUp, MigrateDownAll, Steps, MigrateForce
    
    - Supports multiple database types via golang-migrate
    
    - Integrates with CLI for database schema management

---
+++ migrations.go
@@ -0,0 +1,64 @@
+package celeritas
+
+import (
+	"errors"
+	"github.com/golang-migrate/migrate/v4"
+
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/golang-migrate/migrate/v4/database/mongodb"
+	_ "github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func (c *Celeritas) MigrateUp(dsn string) error {
+	m, err := migrate.New("file://"+c.RootPath+"/migrations", dsn)
+	if err != nil {
+		return err
+	}
+	defer m.Close()
+
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return err
+	}
+	return nil
+}
+
+func (c *Celeritas) MigrateDownAll(dsn string) error {
+	m, err := migrate.New("file://"+c.RootPath+"/migrations", dsn)
+	if err != nil {
+		return err
+	}
+	defer m.Close()
+
+	if err := m.Down(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return err
+	}
+	return nil
+}
+
+func (c *Celeritas) Steps(n int, dsn string) error {
+	m, err := migrate.New("file://"+c.RootPath+"/migrations", dsn)
+	if err != nil {
+		return err
+	}
+	defer m.Close()
+
+	if err := m.Steps(n); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return err
+	}
+	return nil
+}
+
+func (c *Celeritas) MigrateForce(dsn string) error {
+	m, err := migrate.New("file://"+c.RootPath+"/migrations", dsn)
+	if err != nil {
+		return err
+	}
+	defer m.Close()
+	if err := m.Force(-1); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return err
+	}
+	return nil
+}

2e83da250ef235912f87d6ddad81f9bdbacd96ca
JSader <jorge.sader@gmail.com>
2025-03-13 08:57:42 +0100

    feat(cli): add DSN generation and expanded help in helpers
    - Added getDSN() to build database connection strings in cmd/cli/helpers.go
    
    - Enhanced showHelp() with migration commands and moved from main.go
    
    - Lowercased DATABASE_TYPE in setup() for consistency
    
    - Supports PostgreSQL, MySQL, with placeholders for other DBs

--- cmd/cli/helpers.go
+++ cmd/cli/helpers.go
@@ -1,8 +1,13 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
+	"errors"
+	"fmt"
 	"os"
+	"strings"
+
+	"github.com/fatih/color"
+	"github.com/joho/godotenv"
 )
 
 func setup() {
@@ -17,5 +22,60 @@ func setup() {
 	}
 
 	cel.RootPath = path
-	cel.DB.DataType = os.Getenv("DATABASE_TYPE")
+	cel.DB.DataType = strings.ToLower(os.Getenv("DATABASE_TYPE"))
+}
+
+func getDSN() string {
+	dbType := cel.DB.DataType
+
+	var dsn string
+
+	switch dbType {
+	case "pgx", "postgres", "postgresql":
+		if os.Getenv("DATABASE_PASS") != "" {
+			dsn = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
+				os.Getenv("DATABASE_USER"),
+				os.Getenv("DATABASE_PASS"),
+				os.Getenv("DATABASE_HOST"),
+				os.Getenv("DATABASE_PORT"),
+				os.Getenv("DATABASE_NAME"),
+				os.Getenv("DATABASE_SSL_MODE"),
+			)
+		} else {
+			dsn = fmt.Sprintf("postgres://%s@%s:%s/%s?sslmode=%s",
+				os.Getenv("DATABASE_USER"),
+				os.Getenv("DATABASE_HOST"),
+				os.Getenv("DATABASE_PORT"),
+				os.Getenv("DATABASE_NAME"),
+				os.Getenv("DATABASE_SSL_MODE"),
+			)
+		}
+		return dsn
+
+	case "mysql", "mariadb":
+		return "mysql://" + cel.BuildDSN()
+
+	case "sql", "sqlite", "sqlite3", "turso":
+		//TODO: build dsn
+
+	case "mongo", "mongodb":
+	//TODO: build dsn
+
+	default:
+		exitGracefully(errors.New("database type not supported: " + dbType))
+	}
+	return dsn
+}
+
+func showHelp() {
+	color.Yellow(`Available commands:
+
+	help                    - show this help
+	version	                - show version
+	migrate                 - runs all up migrations that have not been applied
+	migrate down            - reverses the most recent migration
+	migrate reset           - runs all down migrations in reverse order, and then all up migrations
+	make migration <name>   - creates two new migrations(one up & one down) in the migrations folder
+
+	`)
 }

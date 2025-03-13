883a58ee688f22c0de2ab5097a6852b2113be46b
JSader <jorge.sader@gmail.com>
2025-03-13 11:09:02 +0100

    feat(cli): add file existence check to copyFileFromTemplate
    - Implemented fileExists() in cmd/cli/copy-files.go
    
    - Updated copyFileFromTemplate() to prevent overwriting existing files
    
    - Resolves TODO by adding error handling for existing files

--- cmd/cli/copy-files.go
+++ cmd/cli/copy-files.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"embed"
+	"errors"
 	"io/ioutil"
+	"os"
 )
 
 //go:embed templates
 var templateFS embed.FS
 
 func copyFileFromTemplate(templatePath string, targetFile string) error {
-	// TODO: check to ensure file doesn't exist
+	if fileExists(targetFile) {
+		return errors.New(targetFile + " already exists!")
+	}
 
 	data, err := templateFS.ReadFile(templatePath)
 	if err != nil {
@@ -28,4 +32,15 @@ func copyDataToFile(data []byte, to string) error {
 		return err
 	}
 	return nil
+}
+
+func fileExists(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+		return false
+	}
+	return true
 }

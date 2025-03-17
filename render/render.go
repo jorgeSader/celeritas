5bb5aab90353549699c6e9b26735c38071fb4c14
JSader <jorge.sader@gmail.com>
2025-03-17 18:48:35 +0100

    feat: add template caching for Go templates in renderer
    Enhances render/render.go with template caching:
    - Adds TemplateCache field (map[string]*template.Template) and UseCache bool to Render struct.
    - Implements CreateTemplateCache to pre-parse all .page.tmpl files and layouts, storing them in a cache.
    - Updates GoPage to use the cache if enabled, falling back to dynamic parsing if disabled, and buffers output.
    - Refactors Page to fix argument order (data, variables) for consistency across renderers.
    - Improves error handling with logging in GoPage.
    
    This improves performance by caching Go templates and enhances rendering reliability.

--- render/render.go
+++ render/render.go
@@ -1,11 +1,13 @@
 package render
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 	"strings"
 
 	"github.com/CloudyKit/jet/v6"
@@ -13,13 +15,15 @@ import (
 )
 
 type Render struct {
-	Renderer   string
-	RootPath   string
-	Secure     bool
-	Port       string
-	ServerName string
-	JetViews   *jet.Set
-	Session    *scs.SessionManager
+	Renderer      string
+	RootPath      string
+	Secure        bool
+	Port          string
+	ServerName    string
+	JetViews      *jet.Set
+	Session       *scs.SessionManager
+	TemplateCache map[string]*template.Template
+	UseCache      bool
 }
 
 type TemplateData struct {
@@ -45,41 +49,62 @@ func (c *Render) defaultData(td *TemplateData, r *http.Request) *TemplateData {
 	return td
 }
 
-func (c *Render) Page(w http.ResponseWriter, r *http.Request, view string, variables, data interface{}) error {
+func (c *Render) Page(w http.ResponseWriter, r *http.Request, view string, data, variables interface{}) error {
 	switch strings.ToLower(c.Renderer) {
 	case "go":
 		return c.GoPage(w, r, view, data)
 	case "jet":
-		return c.JetPage(w, r, view, variables, data)
+		return c.JetPage(w, r, view, data, variables)
 	default:
-
+		return errors.New("no rendering engine specified")
 	}
-	return errors.New("no rendering engine specified")
 }
 
-// GoPage renders a standard Go template
+// GoPage renders a standard Go template using the pre-cached template
 func (c *Render) GoPage(w http.ResponseWriter, r *http.Request, view string, data interface{}) error {
-	tmpl, err := template.ParseFiles(fmt.Sprintf("%s/views/%s.page.tmpl", c.RootPath, view))
-	if err != nil {
-		return err
+	var tc map[string]*template.Template
+	var err error
+
+	if c.UseCache {
+		tc = c.TemplateCache
+	} else {
+		tc, err = c.CreateTemplateCache()
+		if err != nil {
+			log.Printf("Error creating template cache: %v", err)
+			return err
+		}
+	}
+
+	// Get requested template from cache
+	tmpl, ok := tc[view+".page.tmpl"]
+	if !ok {
+		return fmt.Errorf("can't get template %s.page.tmpl from cache", view)
 	}
 
 	td := &TemplateData{}
 	if data != nil {
 		td = data.(*TemplateData)
 	}
-
 	td = c.defaultData(td, r)
 
-	err = tmpl.Execute(w, &td)
+	buf := new(bytes.Buffer)
+	err = tmpl.Execute(buf, td)
 	if err != nil {
+		log.Printf("Error executing template: %v", err)
 		return err
 	}
+
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		log.Printf("Error writing template to browser: %v", err)
+		return err
+	}
+
 	return nil
 }
 
 // JetPage renders a template using the Jet templating engine
-func (c *Render) JetPage(w http.ResponseWriter, r *http.Request, templateName string, variables, data interface{}) error {
+func (c *Render) JetPage(w http.ResponseWriter, r *http.Request, templateName string, data, variables interface{}) error {
 	var vars jet.VarMap
 	if variables == nil {
 		vars = make(jet.VarMap)
@@ -107,4 +132,41 @@ func (c *Render) JetPage(w http.ResponseWriter, r *http.Request, templateName st
 	}
 
 	return nil
+}
+
+// CreateTemplateCache initializes a template cache by parsing all template files
+func (c *Render) CreateTemplateCache() (map[string]*template.Template, error) {
+	templateCache := make(map[string]*template.Template)
+
+	pathToPages := fmt.Sprintf("%s/views/*.page.tmpl", c.RootPath)
+	pathToLayouts := fmt.Sprintf("%s/views/layouts/*.layout.tmpl", c.RootPath)
+
+	pages, err := filepath.Glob(pathToPages)
+	if err != nil {
+		return templateCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return templateCache, err
+		}
+
+		layouts, err := filepath.Glob(pathToLayouts)
+		if err != nil {
+			return templateCache, err
+		}
+
+		if len(layouts) > 0 {
+			ts, err = ts.ParseGlob(pathToLayouts)
+			if err != nil {
+				return templateCache, err
+			}
+		}
+
+		templateCache[name] = ts
+	}
+
+	return templateCache, nil
 }
